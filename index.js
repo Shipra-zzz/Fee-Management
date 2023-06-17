fd3566ac5119ba0b86786dbcfd940694fd856ec7
Shipra-zzz <97395310+Shipra-zzz@users.noreply.github.com>
2023-06-17 14:32:19 +0530

    Update index.js
    missed a quotation mark

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var numberOfButtons=document.querySelectorAll(".drum").length;
-alert("Click the buttons or enter the keys 'w','a','s','d','j','k','l' to play the DRUM!!!);
+alert("Click the buttons or enter the keys 'w','a','s','d','j','k','l' to play the DRUM!!!");
 for(var i=0; i<numberOfButtons; i++){ 
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
